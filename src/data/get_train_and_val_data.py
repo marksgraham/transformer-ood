2576b2db07085e9e9681a85f816f602bd0861e0e
Mark Graham <markgraham539@gmail.com>
2023-02-10 15:43:26 -0600

    Initial commit

---
+++ src/data/get_train_and_val_data.py
@@ -0,0 +1,107 @@
+from pathlib import Path
+
+import torch.distributed as dist
+from monai import transforms
+from monai.data import CacheDataset, Dataset, ThreadDataLoader, partition_dataset
+
+
+def get_data_dicts(ids_path: str, shuffle: bool = False):
+    train_data_list = list(Path(ids_path).glob("*.nii*"))
+    data_dicts = [{"image": d} for d in train_data_list]
+    if shuffle:
+        data_dicts = shuffle(data_dicts)
+
+    print(f"Found {len(data_dicts)} subjects.")
+    if dist.is_initialized():
+        print(dist.get_rank())
+        print(dist.get_world_size())
+        return partition_dataset(
+            data=data_dicts,
+            num_partitions=dist.get_world_size(),
+            shuffle=True,
+            seed=0,
+            drop_last=False,
+            even_divisible=True,
+        )[dist.get_rank()]
+    else:
+        return data_dicts
+
+
+def get_training_data_loader(
+    batch_size: int,
+    training_ids: str,
+    validation_ids: str,
+    only_val: bool = False,
+    augmentation: bool = True,
+    drop_last: bool = False,
+    num_workers: int = 8,
+    num_val_workers: int = 3,
+    cache_data=True,
+    first_n=None,
+    is_grayscale=False,
+    add_vflip=False,
+    add_hflip=False,
+):
+    train_transforms = transforms.Compose(
+        [
+            transforms.LoadImaged(keys=["image"]),
+            transforms.AddChanneld(keys=["image"]),
+            transforms.ScaleIntensityd(keys=["image"]),
+            transforms.CenterSpatialCropd(keys=["image"], roi_size=[176, 208, 176]),
+        ]
+    )
+
+    val_transforms = transforms.Compose(
+        [
+            transforms.LoadImaged(keys=["image"]),
+            transforms.AddChanneld(keys=["image"]),
+            transforms.ScaleIntensityd(keys=["image"]),
+            transforms.CenterSpatialCropd(keys=["image"], roi_size=[176, 208, 176]),
+        ]
+    )
+
+    val_dicts = get_data_dicts(validation_ids, shuffle=False)
+    if cache_data:
+        val_ds = CacheDataset(
+            data=val_dicts,
+            transform=val_transforms,
+        )
+    else:
+        val_ds = Dataset(
+            data=val_dicts,
+            transform=val_transforms,
+        )
+    print(val_ds[0]["image"].shape)
+    val_loader = ThreadDataLoader(
+        val_ds,
+        batch_size=batch_size,
+        num_workers=num_val_workers,
+        drop_last=drop_last,
+        pin_memory=False,
+    )
+
+    if only_val:
+        return val_loader
+
+    train_dicts = get_data_dicts(training_ids, shuffle=False)
+
+    if cache_data:
+        train_ds = CacheDataset(
+            data=train_dicts,
+            transform=train_transforms,
+        )
+    else:
+        train_ds = Dataset(
+            data=train_dicts,
+            transform=train_transforms,
+        )
+    train_loader = ThreadDataLoader(
+        train_ds,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=drop_last,
+        pin_memory=False,
+    )
+
+    return train_loader, val_loader
---
+++ train.py
@@ -0,0 +1,53 @@
+import argparse
+
+from src.trainers.vqvae import VQVAETrainer
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=2, help="Random seed to use.")
+    parser.add_argument("--output_dir", help="Location for models.")
+    parser.add_argument("--model_name", help="Name of model.")
+    parser.add_argument("--training_dir", help="Location of folder with training niis.")
+    parser.add_argument("--validation_dir", help="Location of folder with validation niis.")
+
+    # model params
+
+    # training param
+    parser.add_argument("--batch_size", type=int, default=4, help="Training batch size.")
+    parser.add_argument("--n_epochs", type=int, default=300, help="Number of epochs to train.")
+    parser.add_argument(
+        "--eval_freq",
+        type=int,
+        default=10,
+        help="Number of epochs to between evaluations.",
+    )
+
+    parser.add_argument("--num_workers", type=int, default=8, help="Number of loader workers")
+    parser.add_argument(
+        "--cache_data",
+        type=int,
+        default=1,
+        help="Whether or not to cache data in dataloaders.",
+    )
+    parser.add_argument(
+        "--checkpoint_every",
+        type=int,
+        default=50,
+        help="Save a checkpoint every checkpoint_every epochs.",
+    )
+    parser.add_argument(
+        "--quick_test",
+        default=0,
+        type=int,
+        help="If True, runs through a single batch of the train and eval loop.",
+    )
+    args = parser.parse_args()
+    return args
+
+
+# to run using DDP, run torchrun --nproc_per_node=1 --nnodes=1 --node_rank=0  train.py --args
+if __name__ == "__main__":
+    args = parse_args()
+    trainer = VQVAETrainer(args)
+    trainer.train(args)
